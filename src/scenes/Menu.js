122c7fb00bc89b242781c54b7e45bf28036d6924
Cassian Jones <stealfdragon@gmail.com>
2025-03-05 17:22:34 -0800

    added some keyinputs to menu and also added france

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -4,11 +4,15 @@ class Menu extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('rocket', './assets/rocket.png')
+        this.load.image('reticle', './assets/img/TempDominationReticle.png')
+        this.load.image('France', './assets/map/TempFrance.png')
     }
 
     create() {
-        
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT)
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT)
+        keyUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP)
+        keyDOWN = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN)
     }
 
     update() {
