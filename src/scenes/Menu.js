f24105295ffd7c6c49e82476b5bc7f5609666dfa
Evan Paul Lara <evplara@ucsc.edu>
2025-03-11 16:59:26 -0700

    Added second screen, scoring

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -6,6 +6,8 @@ class Menu extends Phaser.Scene {
     preload() {
         this.load.image('reticle', './assets/img/TempDominationReticle.png')
         this.load.image('France', './assets/map/TempFrance.png')
+        //Sound Effect by <a href="https://pixabay.com/users/ribhavagrawal-39286533/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=230500">Ribhav Agrawal</a> from <a href="https://pixabay.com/sound-effects//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=230500">Pixabay</a>
+        // this.load.audio()
         this.delaunator = 'window.Delaunator';
     }
 
